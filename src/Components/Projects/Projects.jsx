ac29cd9f0b75420b392d2eebc8369b1c32b5154e
Mohd Amir <Amirkhan98375@gmail.com>
2022-05-21 18:05:12 +0530

    project

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -48,19 +48,23 @@ const Projects = () => {
             ],
         },
         {
-            id: 2,
-            project_name: 'Purple Clone',
+            id: 3,
+            project_name: 'Modesens Clone',
             image: 'https://raw.githubusercontent.com/Amir98375/portfolio-amir/master/public/ProjectImages/purple.JPG',
             project_desc:
                 'This is a clone of purple.com an e-commerce american based company which deals in the sale purcahse of beauty products.',
             deploy_link: 'https://bejewelled-taffy-26021a.netlify.app/',
-            repo_link: 'https://github.com/Amir98375/purpleclone',
+            repo_link: 'https://github.com/SumanJK/Modesens-clone-web15',
             tech_stack: [
+               
+                <FaNodeJs />,
+                <SiHeroku/>,
+                <SiRedux />,
+                <SiReact/>,
                 <FaHtml5 />,
                 <FaCss3 />,
                 <FaJs />,
-                <FaNodeJs />,
-                <SiHeroku/>,
+                <SiMaterialui />,
                
            
             ],
